77e164f94239b6b4c330185cdb0eec02ad00b2af
Jüri Valdmann <juri.valdmann@gmail.com>
2025-02-13 15:22:10 +0100

    Enable icons in magit

--- init.el
+++ init.el
@@ -330,6 +330,7 @@
         ("g m m" . magit)
         ("g m b" . magit-blame))
   :config
+  (setopt magit-format-file-function #'magit-format-file-all-the-icons)
   (magit-add-section-hook 'magit-status-sections-hook
                           'magit-insert-modules
                           'magit-insert-stashes
