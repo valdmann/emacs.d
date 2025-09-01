b05d8a193923875d4f71c33751a5f1f9dd83f409
Jüri Valdmann <juri.valdmann@gmail.com>
2025-09-01 20:39:46 +0200

    Remove minuet

--- init.el
+++ init.el
@@ -325,48 +325,6 @@
               ("M-A" . marginalia-cycle))
   :init (marginalia-mode 1))
 
-(use-package minuet
-    :bind
-    (("M-y" . #'minuet-complete-with-minibuffer) ;; use minibuffer for completion
-     ("M-i" . #'minuet-show-suggestion) ;; use overlay for completion
-     ("C-c m" . #'minuet-configure-provider)
-     :map minuet-active-mode-map
-     ;; These keymaps activate only when a minuet suggestion is displayed in the current buffer
-     ("M-p" . #'minuet-previous-suggestion) ;; invoke completion or cycle to next completion
-     ("M-n" . #'minuet-next-suggestion) ;; invoke completion or cycle to previous completion
-     ("<tab>" . #'minuet-accept-suggestion) ;; accept whole completion
-     ;; Accept the first line of completion, or N lines with a numeric-prefix:
-     ;; e.g. C-u 2 M-a will accepts 2 lines of completion.
-     ("M-a" . #'minuet-accept-suggestion-line)
-     ("M-e" . #'minuet-dismiss-suggestion))
-
-    :init
-    ;; if you want to enable auto suggestion.
-    ;; Note that you can manually invoke completions without enable minuet-auto-suggestion-mode
-    (add-hook 'prog-mode-hook #'minuet-auto-suggestion-mode)
-
-    :config
-    (setq minuet-provider 'openai-fim-compatible)
-    (setq minuet-n-completions 1)
-    (setq minuet-context-window 4096)
-    (plist-put minuet-openai-fim-compatible-options :end-point "http://localhost:9292/v1/completions")
-    (plist-put minuet-openai-fim-compatible-options :name "Llama.cpp")
-    (plist-put minuet-openai-fim-compatible-options :api-key "TERM")
-    (plist-put minuet-openai-fim-compatible-options :model "FIM")
-    (minuet-set-nested-plist minuet-openai-fim-compatible-options nil :template :suffix)
-    (minuet-set-optional-options
-     minuet-openai-fim-compatible-options
-     :prompt
-     (defun minuet-llama-cpp-fim-qwen-prompt-function (ctx)
-       (format "<|repo_name|>%s<|file_sep|>%s<|fim_prefix|>%s<|fim_suffix|>%s<|fim_middle|>"
-               (plist-get ctx :repo-path)
-               (plist-get ctx :file-path)
-               (plist-get ctx :before-cursor)
-               (plist-get ctx :after-cursor)))
-     :template)
-
-    (minuet-set-optional-options minuet-openai-fim-compatible-options :max_tokens 100))
-
 (use-package mixed-pitch
   :hook
   (text-mode . mixed-pitch-mode))
